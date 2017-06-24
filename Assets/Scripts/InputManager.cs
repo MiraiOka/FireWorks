9ff68dd23d37a10cea70996a549e36baa0dfc313
岡実来 <MIRAI@MacBook-Pro-3.local>
2017-06-24 23:16:42 +0900

    refactoring

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	[SerializeField] private float coolTime;
+	private float time = 0f;
+	[SerializeField] private SeedManager seedController;
+
+	private void Update ()
+	{
+		time += Time.deltaTime;
+
+		if (Input.GetMouseButton (0) && time > coolTime) {
+			seedController.onFire ();
+			time = 0f;
+		}
+	}
+
+}
---
+++ Assets/Scripts/SeedManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedManager : MonoBehaviour {
+
+	[SerializeField] GameObject seed;
+	[SerializeField] AudioClip audioClip;
+	private AudioSource audio;
+
+	private void Start()
+	{
+		audio = GetComponent<AudioSource> ();
+	}
+
+	public void onFire()
+	{
+		Vector3 clickPosition = Input.mousePosition;
+		GameObject fire = Instantiate (seed, new Vector3(Camera.main.ScreenToViewportPoint(clickPosition).x * 55 - 27, 0, 40), Quaternion.Euler(-90, 0, 0));
+		fire.transform.parent = transform;
+		Destroy (fire, 5);
+		Invoke ("OnAudio", 3.0f);
+	}
+
+	private void OnAudio()
+	{
+		audio.PlayOneShot (audioClip);
+	}
+
+
+}
